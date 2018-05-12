30ad8c1297d46c3b7eaebe5de774fe140fdc7910
CQThang <thanglatoi.toilathang@gmail.com>
2018-05-12 18:00:21 +0700

    cho Y

--- backendapp/crtPostProcess.js
+++ backendapp/crtPostProcess.js
@@ -1,12 +1,11 @@
 module.exports=function (urlencodedParser,app) {
 	app.set("view engine","ejs");
 	app.set("views","../frontend/ejs/");
-	app.route("/createPost")
+	app.route("/createPost/process")
 		.post(urlencodedParser, function(req,res){
 			
-			res.render("CreatePost");
-			console.log(req.body);//req.body chua noi dung tu front gui den
 			
 			
+			res.redirect("/view/IDpost");
 		});
 }
