1820458f8413dcdc658c156f1009559459f911ca
NPLinhUET <lumerijirinmo@gmail.com>
2018-05-13 12:47:42 +0700

    process của Linh (gồm search, view, like, rate)
    pro = new process();
    search: app.get('/search', pro.search);
    view: app.get('/view/:postID', pro.view);
    like: app.get('/view/:postID/:userID', pro.like);
    rate: app.get('/view/:postID/:userID/:score', pro.rate);

--- backendapp/process.js
+++ backendapp/process.js
@@ -27,7 +27,6 @@ module.exports = function(){
 					statement += (i > 0? "and " : "") + 'idPost in (select Post_idPost from ingredieat where name_ingre like "%' + key[i] + '%") ';
 				}
 				console.log(statement);
-				res.redirect('/');
 				con.query(statement, function(err, result){
 					if (err) throw err;
 					if (result.length > 0){
@@ -52,7 +51,7 @@ module.exports = function(){
 				password: "linh7b2ltt",
 				database: "mydb"
 			});
-		var Post = require('../models/post.js');
+		var Post = require('./model/post.js');
 		var post = new Post();
 		con.connect(function(err){
 			if (err) throw err;
@@ -62,16 +61,69 @@ module.exports = function(){
 				if (err) throw err;
 				post.setInfo(result[0].NamePost, result[0].Description, result[0].idPost, result[0].Account_idAccount,
 					result[0].Rating, result[0].Like);
-					console.log(result[0]);
 					console.log(post);
-					//console.log(post.toString);
-				res.send(post.print);
+					console.log(result[0]);
+				res.status(200).send('Rating: '+(post.Rating).toString());
 			});
 		});
 	}
 		
 	this.like = function(req, res){
-		
+		var p = req.params.postID;
+		var u = req.params.userID;
+		var mysql = require('mysql');
+			var con = mysql.createConnection({
+				host: "localhost",
+				user: "Lumeri",
+				password: "linh7b2ltt",
+				database: "mydb"
+			});
+		con.connect(function(err){
+			if (err) throw err;
+			var statement1 = 'insert into likestate (Account_idAccount, Post_idPost) values ('+u+', '+p+')';
+			console.log(statement1);
+			con.query(statement1, function(err){
+				if (err) throw err;
+				var statement2 = 'update Post set like = like+1 where idPost = '+p;
+				console.log(statement2);
+				con.query(statement2, function(err){
+					if (err) throw err;
+					res.redirect('/view/'+p);
+				});
+			})
+		})
+	}
+	
+	this.rate = function(req, res){
+		var u = req.params.userID;
+		var p = req.params.postID;
+		var s = req.params.score;
+		var mysql = require('mysql');
+			var con = mysql.createConnection({
+				host: "localhost",
+				user: "Lumeri",
+				password: "linh7b2ltt",
+				database: "mydb"
+			});
+		con.connect(function(err){
+			if (err) throw err;
+			var statement1 = 'insert into rate (Account_idAccount, Post_idPost, score) values ('+u+', '+p+', '+s+')';
+			console.log(statement1);
+			con.query(statement1, function(err){
+				if (err) throw err;
+				var statement2 = 'select (sum(score)/count(*)) avg from rate group by Post_idPost having Post_idPost = '+p;
+				console.log(statement2);
+				con.query(statement2, function(err, result){
+					if (err) throw err;
+					var statement3 = 'update post set rating = '+result[0].avg+' where idPost = '+p;
+					console.log(statement3);
+					con.query(statement3, function(err){
+						if (err) throw err;
+						res.redirect('/view/'+p);
+					});
+				});
+			});
+		});
 	}
 }
 
