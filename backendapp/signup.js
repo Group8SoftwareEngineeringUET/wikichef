7f0d6f5f82c3401e6f7e004cff03303bd5a656e8
Linh Còi <rainyboy.165@gmail.com>
2018-05-13 15:49:00 +0700

    update sign up

--- backendapp/signup.js
+++ backendapp/signup.js
@@ -7,11 +7,11 @@ module.exports=function (urlencodedParser,app) {
 	app.route("/signup")
 		.post(urlencodedParser, function(req,res){
 			
-			console.log(req.body);//req.body chua noi dung tu front
+			console.log(req.body.email);//req.body chua noi dung tu front
 			//Viet code o day
-			if (!req.body.email || !req.body.pwd || !req.body.rpwd) {
-				console.log('All fields required, please try again!');
-			} else if (req.body.pwd != req.body.rpwd) {
+			if (!req.body.email || !req.body.psw || !req.body.rpsw) {
+				console.log('All fields required. Please enter again!');
+			} else if (req.body.psw != req.body.rpsw) {
 				console.log('Password not match!');
 			} else {
 				var con = mysql.createConnection({
@@ -24,22 +24,26 @@ module.exports=function (urlencodedParser,app) {
 				var query = 'select * from Account where User = "' + req.body.email +'";';
 				con.connect(function(err) {
 					if (err) throw err;
+					//kiểm tra email đã được đăng kí chưa
 					con.query(query, function (err, result) {
 						if (err) throw err;
+						//kết quả tìm được != 0 -> đã tồn tại email
 						if (result.length != 0) {
 							console.log('Email existed!');
 						} else {
-							var statement = 'insert into Account (User, Pass) values (' + req.body.email + ', ' + req.body.pwd + ');';
+							//tạo đối tượng
+							newAccount.setInfo(req.body.email, req.body.psw);
+							var statement = 'insert into Account (User, Pass, Admin) values ("' + req.body.email + '", "' + req.body.psw + '", "' +  newAccount.Admin + '");';
+							//thêm vào CSDL
 							con.query(statement, function (err, result) {
 								if (err) throw err;
+								//kiểm tra lại xem tài khoản đã đc thêm vào CSDL hay chưa
 								con.query(query, function (err, result) {
 									if (err) throw err;
 									if (result.length != 0) {
 										console.log('Them tai khoan thanh cong');
-										//tao doi tuong Account vua dang ki
-										newAccount.setInfo(result[0].User, result[0].Pass, result[0].idAccount);
-									}
-									else console.log('Tai khoan chua dc them');
+										newAccount.setIdAccount(result[0].idAccount);										
+									} else console.log('Tai khoan chua dc them');
 								})
 								//console.log('them tai khoan thanh cong');
 							})
